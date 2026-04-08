2aaf541edaafe44f5579ae101d1a34fb76a81baf
Piyush Agarwal <piyushbora1999@gmail.com>
2026-04-07 20:41:02 -0700

    codecrafters submit [skip ci]

--- app/redisKVStore.py
+++ app/redisKVStore.py
@@ -28,7 +28,7 @@ def prepend_items(key, items):
 
     value, _ = store[key]
     for item in items[::-1]:
-        value.append(item)
+        value = [item] + value
     return len(value)
 
 def get_items(key, start, stop):
