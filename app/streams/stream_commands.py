da0038fddd60db60ddba73a2fc50d6476459e7b5
Piyush Agarwal <piyushbora1999@gmail.com>
2026-04-08 18:59:39 -0700

    codecrafters submit [skip ci]

--- app/streams/stream_commands.py
+++ app/streams/stream_commands.py
@@ -1,4 +1,5 @@
 from ..redisKVStore import *
+from ..respParser import *
 
 def Type(requestParams):
 
@@ -6,9 +7,27 @@ def Type(requestParams):
     if (isinstance(value, str)):
         _type = "string"
     elif (isinstance(value, list)):
-        _type = "list"
+        if (isinstance(value[0], dict)):
+            _type = "stream"
+        else:
+            _type = "list"
     else:
         _type = "none"
     response = f"+{_type}\r\n"
     return response
+
+def Xadd(requestParams):
+
+    key = requestParams[0]
+    value = get_key(key)
+    if (value is None):
+        set_key(key, [])
+    elementId = requestParams[1]
+    elementDict = {"id":elementId}
+    i = 2
+    while (i < len(requestParams)):
+        elementDict[requestParams[i]]  = requestParams[i+1]
+        i += 2
+    append_items(key, [elementDict])
+    return formBulkString(elementId)
 
