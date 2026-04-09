ddc1beb7abe6df52e19b4a2a4f9be765565e38cf
Piyush Agarwal <piyushbora1999@gmail.com>
2026-04-08 18:28:15 -0700

    codecrafters submit [skip ci]

--- app/redisKVStore.py
+++ app/redisKVStore.py
@@ -63,11 +63,9 @@ def get_items(key, start, stop):
 def bl_pop(key, timeout):
 
     value = get_key(key)
-    print("Value State is", value)
     with condition:
         if (value == None or len(value) == 0):
-            condition.wait(timeout=timeout)
-    print(value)
+            condition.wait()
     removedItem = remove_items(key, 1)
     return removedItem
 
